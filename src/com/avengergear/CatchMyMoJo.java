162cead609bd5e0416c51c32d15433fd5462c280
AvengerMoJo <avengermojo@gmail.com>
2013-01-10 22:40:45 +0800

    Add preview screen create layout and activity

--- src/com/avengergear/CatchMyMoJo.java
+++ src/com/avengergear/CatchMyMoJo.java
@@ -1,15 +1,41 @@
 package com.avengergear;
 
 import android.app.Activity;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class CatchMyMoJo extends Activity
 {
+
+    private Camera mCamera; 
+    private CatchMyMoJoPreview mPreview; 
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+	mCamera = getCameraInstance();
+
+	mPreview = new CatchMyMoJoPreview(this, mCamera); 
+	FrameLayout preview = (FrameLayout) findViewById(R.id.catchmymojo_preview); 
+	preview.addView(mPreview); 
     }
+
+    public static Camera getCameraInstance(){ 
+	    Camera cam = null; 
+	    try { 
+		    cam = Camera.open();
+		    Camera.Parameters parameters = cam.getParameters();
+		    parameters.set("camera-id", 2); // open the front cam
+		    cam.setParameters(parameters);
+	    } catch (Exception e){
+		    // shit no cam, exit
+	    }
+	    return cam; 
+    }
+    
 }
---
+++ src/com/avengergear/CatchMyMoJoPreview.java
@@ -0,0 +1,56 @@
+package com.avengergear;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.FrameLayout;
+
+import java.io.IOException;
+
+public class CatchMyMoJoPreview extends SurfaceView implements SurfaceHolder.Callback { 
+    private SurfaceHolder mSurfaceHolder; 
+    private Camera mCamera;
+
+    public CatchMyMoJoPreview(Context context, Camera cam){
+        super(context);
+	mCamera = cam;
+
+        mSurfaceHolder = getHolder();
+	mSurfaceHolder.addCallback(this);
+	//mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+	//
+    }
+
+    public void surfaceCreated(SurfaceHolder holder){
+        try{
+            mCamera.setPreviewDisplay(holder);
+	    mCamera.startPreview();
+	} catch (IOException e){
+		// shit can't preview! 
+	}
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder){
+        // don't care for now 
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h){
+        if(mSurfaceHolder.getSurface() == null){
+            // hack it is not here.. why? 
+	    return; 
+	}
+	try{
+            mCamera.stopPreview();
+	} catch (Exception e){
+            // don't care
+	}
+
+	try{
+            mCamera.setPreviewDisplay(mSurfaceHolder);
+	    mCamera.startPreview();
+	} catch (Exception e){
+            // shit..... preview just won't start
+	}
+    }
+}
